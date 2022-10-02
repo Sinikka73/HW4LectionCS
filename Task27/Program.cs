ba5085c2d89ee4651127d2150506d5685f01d542
olga <bend_sinister@mail.ru>
2022-10-02 20:14:52 +0300

    T27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+//число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int sumOfRangesInNumber(int userNumber)
+{
+    int result = 0;
+    userNumber = Math.Abs(userNumber);
+    
+        while (userNumber > 0)
+        {
+            result = result + userNumber%10;
+            userNumber = userNumber / 10;
+        }
+
+    return result;
+}
+
+Console.Write("Введите любое целое число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int sum = sumOfRangesInNumber(userNumber);
+Console.WriteLine($"Сумма цифр в числе {userNumber} равна  {sum}"); 
