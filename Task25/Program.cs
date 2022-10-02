dac3e30c4f893fb595d8b8bb72d1b7bd63d8c2e1
olga <bend_sinister@mail.ru>
2022-10-02 17:07:29 +0300

    T25 нулевая степень доб

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,23 +3,36 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-int exponentiation(int userNumberA, int userNumberB)
+void printInConsoleWithColor(string information, ConsoleColor color)
 {
-    int result = userNumberA;
-    userNumberB = Math.Abs(userNumberB);
-    for(int i = 1; i < userNumberB; i++)
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+
+int exponentiation(int userNumberA, int userNumberB)
+{   
+    int result = 0;
+    if(userNumberB != 0)
     {
-         result = result * userNumberB;
+        result = userNumberA;
+        userNumberB = Math.Abs(userNumberB);
+        for(int i = 1; i < userNumberB; i++)
+        {
+             result = result * userNumberA;
+        }
     }
+    else result = 1;
+
     return result;
 }
 
-Console.Write("Введите число А: ");
+printInConsoleWithColor("Введите число А: ",ConsoleColor.DarkGreen );
 int userNumberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
+printInConsoleWithColor("Введите число B: ",ConsoleColor.DarkGreen );
 int userNumberB = Convert.ToInt32(Console.ReadLine()); 
 double result = exponentiation(userNumberA, userNumberB);
 
 if(userNumberB > 0)
-Console.WriteLine($"Результат возведения числа {userNumberA} в степень {userNumberB} равен {result}");
-else Console.WriteLine($"Результат возведения числа {userNumberA} в степень {userNumberB} равен {1 / result}");
+printInConsoleWithColor($"Результат возведения числа {userNumberA} в степень {userNumberB} равен {result}", ConsoleColor.Green);
+else printInConsoleWithColor($"Результат возведения числа {userNumberA} в степень {userNumberB} равен {1 / result}", ConsoleColor.Green);
